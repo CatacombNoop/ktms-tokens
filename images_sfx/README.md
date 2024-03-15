def4c5192a0dfc80fc242859bed513d8f4dd5c1a
CatacombNoop <52082573+CatacombNoop@users.noreply.github.com>
2024-03-15 16:47:23 +0500

    Update MD again

--- images_sfx/README.md
+++ images_sfx/README.md
@@ -1,4 +1,8 @@
-[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)[Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)[Иконки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_icons/README.md)[Эффекты](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_sfx/README.md)<table><tr>
+[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)
+[Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)
+[Иконки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_icons/README.md)
+[Эффекты](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_sfx/README.md)
+<table><tr>
 <tr>
 <td valign="bottom">
 <img src="./beam.png" width="100" height="100"><br>
--- markdown.js
+++ markdown.js
@@ -7,11 +7,15 @@ const ROOT_DIR = './images_sfx/';
 const README_FILENAME = 'README.md';
 const NB_IMAGES_PER_LINE = 6;
 let nbImages = 0;
-let mdContent = '[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)\
-[Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)\
-[Иконки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_icons/README.md)\
-[Эффекты](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_sfx/README.md)\
-<table><tr>'
+let mdContent = '[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)';
+mdContent += `
+[Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)`;
+mdContent += `
+[Иконки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_icons/README.md)`;
+mdContent += `
+[Эффекты](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_sfx/README.md)`;
+mdContent += `
+<table><tr>`;
 fs.readdirSync(ROOT_DIR).forEach((image) => {
   if (image !== README_FILENAME && image !== 'markdown.js') {
     if (!(nbImages % NB_IMAGES_PER_LINE)) {
