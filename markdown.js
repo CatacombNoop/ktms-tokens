73be9490a3104448ab21f3cc477cec349acbb54e
CatacombNoop <52082573+CatacombNoop@users.noreply.github.com>
2024-03-15 16:44:32 +0500

    Updated Markdown to show links

--- markdown.js
+++ markdown.js
@@ -7,8 +7,11 @@ const ROOT_DIR = './images_sfx/';
 const README_FILENAME = 'README.md';
 const NB_IMAGES_PER_LINE = 6;
 let nbImages = 0;
-let mdContent = '<table><tr>';
-
+let mdContent = '[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)\
+[Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)\
+[Иконки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_icons/README.md)\
+[Эффекты](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_sfx/README.md)\
+<table><tr>'
 fs.readdirSync(ROOT_DIR).forEach((image) => {
   if (image !== README_FILENAME && image !== 'markdown.js') {
     if (!(nbImages % NB_IMAGES_PER_LINE)) {
