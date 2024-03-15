3e8f0065078d7d2a194e8dfd4f41b56666dda1f6
CatacombNoop <52082573+CatacombNoop@users.noreply.github.com>
2024-03-15 18:55:48 +0500

    Sui

--- markdown.js
+++ markdown.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const ROOT_DIR = './images_sfx/';
+const ROOT_DIR = './images_mudrog/';
 const README_FILENAME = 'README.md';
 const NB_IMAGES_PER_LINE = 6;
 let nbImages = 0;
