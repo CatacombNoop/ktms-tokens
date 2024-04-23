b5fc81089816a210b460e9f595bb66271adf5ddd
CatacombNoop <52082573+CatacombNoop@users.noreply.github.com>
2024-04-23 17:18:46 +0500

    выа

--- markdown.js
+++ markdown.js
@@ -3,13 +3,15 @@
 const fs = require('fs');
 const path = require('path');
 const image_folder = [];
-image_folder.push("./images_main/", "./images_mudrog/", "./images_sfx/", "./images_icons/", "./images_icons2/", "./images_odium/", "./wiki_img/", "./images_mark/");
+image_folder.push("./images_main/", "./images_main2/", "./images_mudrog/", "./images_sfx/", "./images_icons/", "./images_icons2/", "./images_odium/", "./wiki_img/", "./images_mark/");
 const ROOT_DIR = './images_';
 const README_FILENAME = 'README.md';
 const NB_IMAGES_PER_LINE = 6;
 image_folder.forEach((element) => {
 let nbImages = 0;
 let mdContent = '[Основные Токены](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main/README.md)';
+mdContent += ` |
+[Основные Токены 2](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_main2/README.md)`;
 mdContent += ` |
 [Мудроградовки](https://github.com/CatacombNoop/ktms-tokens/blob/main/images_mudrog/README.md)`;
 mdContent += ` |
